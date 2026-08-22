fb8732a89c7a78b8fb6f048df1f00014a1dbbb1c
mroisulislamrasel-droid <mroisulislamrasel-droid@users.noreply.github.com>
2026-08-22 06:44:09 +0600

    Test basic Rivo startup

--- app/src/main/java/com/rivo/app/MainActivity.kt
+++ app/src/main/java/com/rivo/app/MainActivity.kt
@@ -1,73 +1,24 @@
 package com.rivo.app
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Toast
+import android.graphics.Color
+import android.view.Gravity
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
-    private lateinit var webView: WebView
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        webView = findViewById(R.id.webview)
-
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            mediaPlaybackRequiresUserGesture = false
-            allowFileAccess = false
-            allowContentAccess = true
-            mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
-            javaScriptCanOpenWindowsAutomatically = true
-            setSupportMultipleWindows(false)
+        val text = TextView(this).apply {
+            text = "Rivo\n\nঅ্যাপ সফলভাবে চালু হয়েছে ✅"
+            textSize = 24f
+            setTextColor(Color.BLACK)
+            gravity = Gravity.CENTER
+            setBackgroundColor(Color.WHITE)
         }
 
-        webView.webViewClient = object : WebViewClient() {
-            override fun onReceivedError(
-                view: WebView,
-                request: WebResourceRequest,
-                error: WebResourceError
-            ) {
-                if (request.isForMainFrame) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Rivo লোড হতে সমস্যা হয়েছে। ইন্টারনেট চেক করুন।",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-        }
-
-        webView.webChromeClient = WebChromeClient()
-
-        webView.loadUrl("https://mroisulislamrasel-droid.github.io/rivo-app/")
-    }
-
-    override fun onDestroy() {
-        webView.stopLoading()
-        webView.webChromeClient = null
-        webView.webViewClient = null
-        webView.destroy()
-        super.onDestroy()
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        setContentView(text)
     }
 }
