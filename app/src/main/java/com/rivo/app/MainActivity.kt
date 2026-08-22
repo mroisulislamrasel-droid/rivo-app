f5830be766e18ffaae2042d9b23d3f7fb3315ffc
mroisulislamrasel-droid <mroisulislamrasel-droid@users.noreply.github.com>
2026-08-22 06:15:20 +0600

    Fix Android WebView startup

--- app/src/main/java/com/rivo/app/MainActivity.kt
+++ app/src/main/java/com/rivo/app/MainActivity.kt
@@ -4,11 +4,15 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : Activity() {
+
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -17,21 +21,53 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webview)
+
         webView.settings.apply {
             javaScriptEnabled = true
             domStorageEnabled = true
             mediaPlaybackRequiresUserGesture = false
-            allowFileAccess = true
+            allowFileAccess = false
             allowContentAccess = true
             mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportMultipleWindows(false)
         }
-        webView.webViewClient = WebViewClient()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                if (request.isForMainFrame) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Rivo লোড হতে সমস্যা হয়েছে। ইন্টারনেট চেক করুন।",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
         webView.webChromeClient = WebChromeClient()
+
         webView.loadUrl("https://mroisulislamrasel-droid.github.io/rivo-app/")
+    }
+
+    override fun onDestroy() {
+        webView.stopLoading()
+        webView.webChromeClient = null
+        webView.webViewClient = null
+        webView.destroy()
+        super.onDestroy()
     }
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
-        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
